caa077438d41a22b330892d7e990db0ed13f7698
Zephaniah <zealorez@gmail.com>
2021-06-15 09:50:13 +0800

    ability to get all items of a trip

--- controllers/items.mjs
+++ controllers/items.mjs
@@ -1,24 +1,41 @@
 export default function initItemsController(db) {
-  // const addItem = async (req, res) => {
-  //   const {
-  //     type, tripId, mainText, secondaryText,
-  //   } = req.body;
+  const addItem = async (req, res) => {
+    const { tripId } = req.params;
+    const {
+      description, place_id, structured_formatting, types,
+    } = req.body.infoObj;
+    const { main_text, secondary_text } = structured_formatting;
+    try {
+      const item = await db.Item.create({
+        description,
+        tripId,
+        type: types,
+        placeId: place_id,
+        mainText: main_text,
+        secondaryText: secondary_text,
+      });
 
-  //   try {
-  //     const item = await db.Item.create({
-  //       name: mainText,
-  //       tripId,
-  //       type,
-  //       address: secondaryText,
-  //     });
+      res.send(item);
+    } catch (err) {
+      console.log('addItem', err);
+    }
+  };
 
-  //     res.send(item);
-  //   } catch (err) {
-  //     console.log('=======addItem err\n========', err);
-  //   }
-  // };
+  const getItems = async (req, res) => {
+    const { tripId } = req.params;
+    try {
+      const items = await db.Item.findAll({
+        where: {
+          tripId,
+        },
+      });
+      res.send(items);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return {
-    // addItem,
+    addItem, getItems,
   };
 }
--- routes.mjs
+++ routes.mjs
@@ -1,11 +1,16 @@
 import db from './models/index.mjs';
 
 // import your controllers here
+import initTripsController from './controllers/trips.mjs';
+import initUsersController from './controllers/users.mjs';
+import initItemsController from './controllers/items.mjs';
 
 export default function bindRoutes(app) {
-
+  const TripsController = initTripsController(db);
+  const UsersController = initUsersController(db);
+  const ItemsController = initItemsController(db);
   // initialize the controller functions here
   // pass in the db for all callbacks
-
-  // define your route matchers here using app
+  app.post('/add_item/:tripId', ItemsController.addItem);
+  app.get('/get_items/:tripId', ItemsController.getItems);
 }
