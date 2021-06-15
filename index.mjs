b3cd8f6ec9884cead4778f3afdd29ed04d27021f
Zephaniah <zealorez@gmail.com>
2021-06-15 09:50:41 +0800

    add json middleware

--- index.mjs
+++ index.mjs
@@ -15,6 +15,8 @@ app.use(cors({
 app.set('view engine', 'ejs');
 // Bind cookie parser middleware to parse cookies in requests
 app.use(cookieParser());
+// Bind Express middleware to parse JSON request bodies
+app.use(express.json());
 // Bind Express middleware to parse request bodies for POST requests
 app.use(express.urlencoded({ extended: false }));
 // Bind method override middleware to parse PUT and DELETE requests sent as POST requests
