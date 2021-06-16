1ef955d810c79955135b19e19509c18be1dc18ed
Zephaniah <zealorez@gmail.com>
2021-06-16 11:27:31 +0800

    change add appoitment to include start and end date

--- controllers/appointments.mjs
+++ controllers/appointments.mjs
@@ -2,19 +2,20 @@ export default function initAppointmentsController(db) {
   const addAppointment = async (req, res) => {
     const { tripId } = req.params;
     const {
-      description, place_id, structured_formatting, types,
-    } = req.body.infoObj;
+      description, place_id, structured_formatting, types, startDate, endDate,
+    } = req.body;
     const { main_text, secondary_text } = structured_formatting;
     try {
-      const appointment = await db.Item.create({
+      const appointment = await db.Appointment.create({
         description,
         tripId,
-        type: types,
+        types,
         placeId: place_id,
+        start_date: new Date(startDate),
+        end_date: new Date(endDate),
         mainText: main_text,
         secondaryText: secondary_text,
       });
-
       res.send(appointment);
     } catch (err) {
       console.log('addAppointment', err);
