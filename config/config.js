6bcf0372cf34afaa209302b00f0b37ad37e286f6
Zephaniah <zealorez@gmail.com>
2021-06-18 07:38:49 +0800

    add configuration for production

--- config/config.js
+++ config/config.js
@@ -6,4 +6,15 @@ module.exports = {
     host: '127.0.0.1',
     dialect: 'postgres',
   },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: { // https://github.com/sequelize/sequelize/issues/12083
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
